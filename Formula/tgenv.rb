b7720e4d9bb4a5beba5f75fd8c36f04391d64feb
alextodicescu <atodicescu@groupon.com>
2019-08-04 17:43:13 +0100

    update url

--- Formula/tgenv.rb
+++ Formula/tgenv.rb
@@ -1,9 +1,9 @@
 class Tgenv < Formula
   desc "Terragrunt version manager inspired by tfenv"
-  homepage "https://github.com/atodicescu/tgenv"
+  homepage "https://github.com/alextodicescu/tgenv"
   url "https://github.com/alextodicescu/tgenv/archive/v0.0.8.tar.gz"
   sha256 "fd129857f55eaf4c3eb89b760668ab10f06ae3bc11c1d832490ef86a4a5cc3f2"
-  head "https://github.com/atodicescu/tgenv.git"
+  head "https://github.com/alextodicescu/tgenv.git"
 
   bottle :unneeded
 
